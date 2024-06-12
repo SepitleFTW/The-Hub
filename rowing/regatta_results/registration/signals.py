ca629393cce436d1df19489eedb3ad4ca9b21bcd
Sepitle Leshilo <136067402+SepitleFTW@users.noreply.github.com>
2024-06-12 22:37:54 +0200

    Update signals.py

--- rowing/regatta_results/registration/signals.py
+++ rowing/regatta_results/registration/signals.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 
 
 @receiver(post_save, sender=User)
-def make_profile(sender, instance, created, **kwargs):
+def make_profile(sender, instance, created, **kwargs): #call this whenever new User obj is saved
     if created:
         Profile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
+def save_profile(sender, instance, **kwargs): #function to save New user
     instance.profile.save()
